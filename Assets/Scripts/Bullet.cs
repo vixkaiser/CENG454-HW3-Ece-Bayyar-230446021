f411e5ad030ec53eba78010d6f255e66f685e6c8
Ece <ecebayyar@gmail.com>
2026-05-17 16:10:30 +0300

    faet: add bullet enemy collision

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
